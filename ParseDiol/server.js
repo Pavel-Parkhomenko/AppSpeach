a5a8941585198a4a9c659906c95f31b1b8ef5da8
Pavel-Parkhomenko <pawelparhomenko@gmail.com>
2025-05-30 15:46:02 +0300

    attempt add read imgs

--- ParseDiol/server.js
+++ ParseDiol/server.js
@@ -47,6 +47,48 @@ app.post('/save-json', (req, res) => {
     });
 });
 
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+app.post("/action-img", (req, res) => {
+    const { folderPath, countUnit } = req.body
+
+    const SOURCE_DIR = 'source_images';
+    const TARGET_DIR = 'processed_images';
+
+    if(fs.existsSync(SOURCE_DIR)) {
+        return res.status(400).json({
+            error: `Папка ${SOURCE_DIR} не существует`
+        })
+    }
+
+    if(fs.existsSync(TARGET_DIR)) {
+        fs.mkdirSync(TARGET_DIR)
+    }
+
+    const files = fs.readFileSync(SOURCE_DIR)
+        .filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file))
+
+    if(files.length === 0) {
+        return res.json({message: 'Нет изображений для обработки'})
+    }
+
+    let randIndexImgs = []
+    for(let i = 0; i < countUnit; i++) {
+        randIndexImgs.push(getRandom(0, files.length - 1))
+    }
+
+    randIndexImgs.map((unit, ind) => {
+        const ext = path.extname(files[unit])
+        const newName = `${ind}`
+        const sourcePath = path.join(SOURCE_DIR, files[ind])
+        
+    })
+})
+
 // Запуск сервера
 app.listen(PORT, () => {
     console.log(`Сервер запущен: http://localhost:${PORT}`);
